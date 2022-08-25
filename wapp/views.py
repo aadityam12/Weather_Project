dfd821650f3d7a25dca1d89b5c92e0ac31c1a1c8
MugdhaSholapure <66936876+MugdhaSholapure@users.noreply.github.com>
2022-08-25 14:15:50 +0530

    Add files via upload

---
+++ wapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def home(request):
+	if request.GET.get("city") and request.GET.get("btn"):
+		city = request.GET.get("city")
+		try:
+			a1 = "http://api.openweathermap.org/data/2.5/weather?units=metric" 
+			a2 = "&q=" + city
+			a3 = "&appid=" + "c6e315d09197cec231495138183954bd"
+			wa = a1 + a2 + a3
+			res = requests.get(wa)
+			data = res.json()
+			temp = data['main']['temp']
+			desc = data['weather'][0]['description']
+			msg = " City = " + city + "\n Temp = " + str(temp) + "\n Desc = " + desc
+			icon = "http://api.openweathermap.org/img/w/" + data['weather'][0]['icon'] + ".png"
+			return render(request,"home.html",{"msg":msg})
+		except Exception as e:
+			return render(request,"home.html",{"msg":str(e) + str("Invalid City Name")})
+	else:
+		return render(request,"home.html")
+
+			
